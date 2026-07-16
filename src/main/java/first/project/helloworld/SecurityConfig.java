090d0c0a3bf85a6a91591a932a1fac681fe416d3
venkateshjk7 <venkatesh2005jk@gmail.com>
2026-07-16 21:48:31 +0530

    Update SecurityConfig.java

--- src/main/java/first/project/helloworld/SecurityConfig.java
+++ src/main/java/first/project/helloworld/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5500","https://todo-frontend-repo-seven.vercel.app"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
 
